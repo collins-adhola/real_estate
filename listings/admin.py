da5805f8c29f031829f9205405e6141439c41b8b
Collino <coadhola@gmail.com>
2021-06-26 16:57:21 +0100

    Dynamic listings

--- listings/admin.py
+++ listings/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Listing
 
+
+
 class ListingAdmin(admin.ModelAdmin):
   list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
   list_display_links = ('id', 'title')
@@ -10,8 +12,11 @@ class ListingAdmin(admin.ModelAdmin):
   search_fields= ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
   list_per_page = 25
 
-
-
 admin.site.register(Listing, ListingAdmin)
+
+
+
+
+
 
 # Register your models here.
